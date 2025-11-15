6ad88155a7971cded5f74b349f879882006035c1
Ed Pedini <epedini@gmail.com>
2025-11-14 16:11:27 -0800

    chore: initial Vite React TS + Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+<div className="p-20 bg-pink-300 text-black">
+  <h1 className="text-3xl font-bold">
+    Tailwind is definitely working!
+  </h1>
+</div>
+  );
+}
+
+export default App;
