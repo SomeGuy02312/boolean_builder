199721c8db5e701cc42a45491239ae015915c576
Ed Pedini <epedini@gmail.com>
2025-11-16 18:46:52 -0800

    fixing root config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/boolean_builder/',   // 👈 IMPORTANT: repo name with leading & trailing slash
+  base: '/',   // 👈 IMPORTANT: repo name with leading & trailing slash
 
 })
